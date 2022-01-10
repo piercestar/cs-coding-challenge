354e4f05333fe4ec16a2e86d97f0140161afe52b
Developer Terence <tko.developer.02@gmail.com>
2022-01-10 17:41:54 +0800

    Updated LineCommandTest

--- app/src/test/java/draw/command/LineCommandTest.java
+++ app/src/test/java/draw/command/LineCommandTest.java
@@ -67,12 +67,12 @@ public class LineCommandTest {
 
         int x1 = 100;
         int y1 = 200;
-        int x2 = 300;
+        int x2 = 100;
         int y2 = 400;
         int height = 4;
         int width = 20;
 
-        String expected = "Only horizontal or vertical lines are allowed." + System.getProperty("line.separator");
+        String expected = "Line exceeds the canvas boundary." + System.getProperty("line.separator");
                         
         Canvas canvas = new Canvas(width, height);
         LineCommand command = new LineCommand(canvas, x1, y1, x2, y2);
